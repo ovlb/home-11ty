808a33fcb5bda3b6d05721ac40d7aa9eefc3e857
Oscar <o@ovl.design>
2020-04-18 22:40:11 +0200

    📚 set article permalink in data

--- _src/_data/articles.js
+++ _src/_data/articles.js
@@ -2,9 +2,20 @@ const getPosts = require('../../_helper/getPosts')
 
 module.exports = async function () {
   const all = await getPosts({ type: 'article', order: '-fields.date' })
-  const ownPosts = all.filter((post) => post.isInternal)
+
+  const posts = all.map((article) => {
+    const isInternal = article.isInternal || !article.externalPost
+
+    article.permalink = isInternal
+      ? `/text/${article.slug}/`
+      : article.externalPost.fields.link
+
+    return article
+  })
+  const ownPosts = posts.filter((post) => post.isInternal)
+
   return {
-    posts: await getPosts({ type: 'article', order: '-fields.date' }),
+    posts,
     ownPosts
   }
 }
