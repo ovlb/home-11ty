d2f26845f591dbbdbd7736699da4a788fe9f94b1
Oscar <o@ovl.design>
2021-12-25 20:04:03 +0100

    🖊 enable attribution && containers

--- _libraries/markdown.js
+++ _libraries/markdown.js
@@ -8,11 +8,17 @@ const anchor = require('markdown-it-anchor')
 const prism = require('markdown-it-prism')
 const footnotes = require('markdown-it-footnote')
 const abbr = require('markdown-it-abbr')
+const attribution = require('markdown-it-attribution')
 
 md.use(anchor)
+md.use(require('markdown-it-container'), 'featured')
 md.use(prism)
 md.use(footnotes)
 md.use(abbr)
+md.use(attribution, {
+  marker: '--',
+  removeMarker: true
+})
 
 md.renderer.rules.footnote_block_open = () =>
   '<section class="footnotes">\n' +
